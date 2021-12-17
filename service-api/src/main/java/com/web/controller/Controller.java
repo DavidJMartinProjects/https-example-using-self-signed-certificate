04759c90b5cceb9f7fada719275952f24cfc1795
David Martin <davidjmartin@hotmail.com>
2021-12-17 18:42:07 +0000

    clean up

--- service-api/src/main/java/com/web/controller/Controller.java
+++ service-api/src/main/java/com/web/controller/Controller.java
@@ -1,8 +1,7 @@
-package com.web.service;
+package com.web.controller;
 
 import com.web.model.MyMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
